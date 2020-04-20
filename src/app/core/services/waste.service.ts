baebe497c03fe6badbe751e7d30266abbdfa899e
Etan Imanol Castro Aldrete <A01209675@itesm.mx>
2020-04-20 09:20:40 -0500

    okay

--- src/app/core/services/waste.service.ts
+++ src/app/core/services/waste.service.ts
@@ -7,7 +7,11 @@ import { Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 const WASTE_TYPE_KEY = '/waste_type';
-
+/**
+ * Castea el snapshot del objecto a uno tipo Waste
+ * @param  {any} fbWaste
+ * @returns WasteType
+ */
 function parseFBPWasteToWaste(fbWaste: any): WasteType {
   const data  = fbWaste.payload.doc.data();
   const id = fbWaste.payload.doc.id;
@@ -23,12 +27,19 @@ function parseFBPWasteToWaste(fbWaste: any): WasteType {
 @Injectable({
   providedIn: 'root'
 })
+/**
+ * Servicio para CRUD del tipo Waste
+ */
 export class WasteService {
 
+  // eslint-disable-next-line require-jsdoc
   constructor(public firedb: AngularFirestore) { }
-
+  /**
+   * Regresa una promesa que regresará una lista de WasteTypes
+   * @returns Promise
+   */
   getWastes(): Promise<WasteType[]> {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       let subscription: Subscription;
       subscription = this.firedb.collection<any>(WASTE_TYPE_KEY).snapshotChanges()
           .pipe(map(snapshot => {
