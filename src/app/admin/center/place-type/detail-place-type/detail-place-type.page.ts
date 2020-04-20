3232b0ebf2374857df282f32dcd1f0d621dd8a64
Ulises Almaguer Guzmán <a01209070@itesm.mx>
2020-04-20 16:15:14 -0500

    CRUD functions of Waste_Types

--- src/app/admin/center/place-type/detail-place-type/detail-place-type.page.ts
+++ src/app/admin/center/place-type/detail-place-type/detail-place-type.page.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { TipoInstalacion } from 'src/app/core/models/tipo-instalacion.model';
 import { PlacesService } from 'src/app/core/services/places.service';
 import { ActivatedRoute } from '@angular/router';
-import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-detail-place-type',
@@ -16,14 +15,11 @@ export class DetailPlaceTypePage implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private placeService: PlacesService,
-    private alertCtrl: AlertController,
-    private navCtrl: NavController
   ) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paraMap => {
       if (!paraMap.has('wasteId')) {
-        // redirect
         return;
       }
       const wasteId = paraMap.get('wasteId');
@@ -33,27 +29,6 @@ export class DetailPlaceTypePage implements OnInit {
           console.log(this.loadedPlaceType);
         });
       }
-    });
-  }
-
-  onDeletePlaceType() {
-    this.alertCtrl.create ({
-      header: '¿Estas segur@?',
-      message: '¿De verdad quieres eliminar este tipo de lugar?',
-      buttons: [{
-        text: 'Cancelar',
-        role: 'cancel'
-      }, {
-        text: 'Borrar',
-        handler: () => {
-          this.placeService.deletePlaceTypeByID(this.loadedPlaceType.id).then(() => {
-            this.navCtrl.navigateBack(['/admin/center/place-type']);
-          })
-              .catch(() => {});
-        }
-      }]
-    }).then(alertEl => {
-      alertEl.present();
     });
   }
 
--- src/app/admin/center/place-type/place-type.page.ts
+++ src/app/admin/center/place-type/place-type.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from 'src/app/core/services/places.service';
+import { ActivatedRoute } from '@angular/router';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-type',
@@ -10,7 +12,11 @@ export class PlaceTypePage implements OnInit {
 
   placeTypes: any [];
 
-  constructor(private placesService: PlacesService) {
+  constructor(  
+    private activatedRoute: ActivatedRoute,
+    private placesService: PlacesService,
+    private alertCtrl: AlertController,
+    private navCtrl: NavController) {
   }
 
   ngOnInit() {
@@ -18,5 +24,26 @@ export class PlaceTypePage implements OnInit {
 
   ionViewWillEnter() {
     this.placesService.allPlaceTypes().then( data => { this.placeTypes = data; });
+  }
+
+  onDeletePlaceType(placeTypeId: string, name: string) {
+    this.alertCtrl.create ({
+      header: 'Mensaje de Confirmación',
+      message: '¿De verdad quieres eliminar el tipo de residuo "'+name + '"?',
+      buttons: [{
+        text: 'Cancelar',
+        role: 'cancel'
+      }, {
+        text: 'Borrar',
+        handler: () => {
+          this.placesService.deletePlaceTypeByID(placeTypeId).then(() => {
+            this.navCtrl.navigateBack(['/admin/center/place-type']);
+          })
+              .catch(() => {});
+        }
+      }]
+    }).then(alertEl => {
+      alertEl.present();
+    });
   }
 }
