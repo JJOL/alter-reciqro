afb2475ce84de205ab6c53cb363a89e4d41e2e9f
Etan Imanol Castro Aldrete <A01209675@itesm.mx>
2020-04-21 23:31:26 -0500

    Saca lo de los usuarios, falta por hacer update

--- src/app/core/services/delegation.service.ts
+++ src/app/core/services/delegation.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import 'firebase/firestore';
 import { Subscription } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map,take } from 'rxjs/operators';
 import { DelegationModel } from '../models/delegation.model';
 
 const DELEGATION_KEY = '/delegation';
@@ -20,11 +20,18 @@ function parseFBPDelegationToDelegation(fbDelegation: any): DelegationModel {
 @Injectable({
   providedIn: 'root'
 })
-
+/**
+ * Service for delegations CRUD 
+ */
 export class DelegationService {
 
+  // eslint-disable-next-line require-jsdoc
   constructor(public firedb: AngularFirestore) { }
 
+  /**
+   * Return all delegations 
+   * @returns Promise
+   */
   getDelegations(): Promise<DelegationModel[]> {
     return new Promise((resolve) => {
       let subscription: Subscription;
@@ -40,5 +47,26 @@ export class DelegationService {
           });
     });
   }
-  
+
+  /**
+   * Return a delegation based on its ID
+   * @param  {string} id
+   * @returns Promise<Place>
+   */
+  getDelegationByID(id: string): Promise<DelegationModel> {
+    return new Promise((resolve) => {
+      let subscription: Subscription;
+      subscription = this.firedb.collection<any>(DELEGATION_KEY).doc<any>(id).valueChanges()
+          .pipe(
+              take(1),
+              map(delegation => {return delegation}))
+          .subscribe(delegation => {
+            if (subscription) {
+              subscription.unsubscribe();
+            }
+            resolve(delegation);
+          });
+    });
+  }
+
 }
