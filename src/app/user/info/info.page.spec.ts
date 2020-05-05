8416d3f6c63aed044cf56c69b10c4e8e95ae0270
jjol <jjoulk@gmail.com>
2020-05-05 18:00:35 -0500

    Arreglar Tests

--- src/app/user/info/info.page.spec.ts
+++ src/app/user/info/info.page.spec.ts
@@ -4,6 +4,77 @@ import { IonicModule } from '@ionic/angular';
 import { InfoPage } from './info.page';
 import { SharedPageModule } from 'src/app/shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { BehaviorSubject, empty } from 'rxjs';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthService } from 'src/app/core/services/auth.service';
+
+
+const authStub: any = {
+  authState: {},
+  auth: {
+    signInWithEmailAndPassword() {
+      return Promise.resolve();
+    }
+  }
+};
+
+
+const mockAuthentication ={
+  registerUser: () => {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  },
+  loginEmailUser: () => {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  },
+  loginGoogleUser: () => {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  },
+  logoutUser: () => {
+    return [];
+  },
+  isAuth:  () => {
+    return empty();
+  },
+  updateUserData: () => {
+    return [];
+  },
+  getCurrentUser : () => {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  },
+  updateCurrentUser: () => {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  },
+  sendPasswordResetEmail: () => {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  },
+  updateUserByUID: () => {
+    return [];
+  },
+  getUserByUID: () => {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  },
+  createUser: ()=> {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  },
+  isUserLoggedIn: new BehaviorSubject(false),
+  userRoles: new BehaviorSubject([]),
+};
 
 describe('InfoPage', () => {
   let component: InfoPage;
@@ -13,6 +84,9 @@ describe('InfoPage', () => {
     TestBed.configureTestingModule({
       declarations: [ InfoPage ],
       imports: [IonicModule.forRoot(), RouterModule.forRoot([]), SharedPageModule],
+      providers: [
+        { provide: AngularFireAuth, useValue: authStub },
+        { provide: AuthService, useValue: mockAuthentication }]
     }).compileComponents();
 
     fixture = TestBed.createComponent(InfoPage);
