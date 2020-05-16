6321c1cf287a1b2f3be2074037948c936da82f75
Juan David Torres <juandatorr_1999@hotmail.com>
2020-05-15 21:22:38 -0500

    RBAC en admin/center con usuarios

--- src/app/admin/center/center.page.ts
+++ src/app/admin/center/center.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../../core/services/auth.service';
 @Component({
   selector: 'app-center',
   templateUrl: './center.page.html',
@@ -10,6 +10,22 @@ import { Component, OnInit } from '@angular/core';
  * Class for Angular CenterPage module.
  */
 export class CenterPage implements OnInit {
+  isLogged: boolean;
+  roles:string[]
+  staff: boolean;
+  user: boolean;
+  /**
+   * Constructor for the center admin page
+   * @param  {AuthService} privateauthService
+   */
+  constructor(private authService:AuthService){
+    this.authService.isUserLoggedIn.asObservable().subscribe(value => {
+      this.isLogged = value;
+    });
+    this.authService.userRoles.asObservable().subscribe(roles => {
+      this.roles=roles
+    });
+  }
   /**
    *  Function not implemented but forced by angular.
    */
