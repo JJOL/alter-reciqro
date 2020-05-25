d62bde5e465e501e809e1afc24f36f9c68a27740
Juan David Torres <juandatorr_1999@hotmail.com>
2020-05-25 01:11:55 -0500

    toolbar

--- src/app/admin/center/list-center/detail-center/detail-center.page.spec.ts
+++ src/app/admin/center/list-center/detail-center/detail-center.page.spec.ts
@@ -4,8 +4,69 @@ import { IonicModule } from '@ionic/angular';
 import { CenterDetailPage } from './detail-center.page';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { from } from 'rxjs';
+import { from, BehaviorSubject, empty } from 'rxjs';
 import { GoogleMapComponent } from 'src/app/shared/google-map/google-map.component';
+import { SharedPageModule } from 'src/app/shared/shared.module';
+import { AuthService } from '../../../../core/services/auth.service';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+
+const mockAuthentication ={
+  registerUser: () => {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  },
+  loginEmailUser: () => {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  },
+  loginGoogleUser: () => {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  },
+  logoutUser: () => {
+    return [];
+  },
+  isAuth:  () => {
+    return empty();
+  },
+  updateUserData: () => {
+    return [];
+  },
+  getCurrentUser : () => {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  },
+  updateCurrentUser: () => {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  },
+  sendPasswordResetEmail: () => {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  },
+  updateUserByUID: () => {
+    return [];
+  },
+  getUserByUID: () => {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  },
+  createUser: ()=> {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  },
+  isUserLoggedIn: new BehaviorSubject(false),
+  userRoles: new BehaviorSubject([]),
+};
+
 
 const arr = [[]];
 
@@ -25,10 +86,11 @@ describe('CenterDetailPage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CenterDetailPage, GoogleMapComponent ],
-      imports: [IonicModule.forRoot(), RouterTestingModule],
+      declarations: [ CenterDetailPage ],
+      imports: [IonicModule.forRoot(), RouterTestingModule,SharedPageModule],
       providers: [
-        { provide: AngularFirestore, useValue: angularFirestoreStub }
+        { provide: AngularFirestore, useValue: angularFirestoreStub },
+        { provide: AuthService, useValue: mockAuthentication }
       ]
     }).compileComponents();
 
