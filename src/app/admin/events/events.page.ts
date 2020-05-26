8699f0973061b87a3495fc824697f3f68507259a
Etan Imanol Castro Aldrete <A01209675@itesm.mx>
2020-05-25 22:03:15 -0500

    arreglos

--- src/app/admin/events/events.page.ts
+++ src/app/admin/events/events.page.ts
@@ -55,7 +55,7 @@ export class EventsPage implements OnInit {
             this.eventService.getAllEvents().then(events => {
               this.showToast('Se eliminó de manera correcta');
               this.events = events;
-              this.events = this.events.filter(event => event.end_date >= this.todayDate);
+              this.listEvents = this.events.filter(event => event.end_date >= this.todayDate);
             });
           }).catch( () => {
             this.showToastWrong('Error al crear el evento');
