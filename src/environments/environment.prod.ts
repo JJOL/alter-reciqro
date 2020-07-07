6b23fe34eacb8cb9a544f33bc7d2fde512aab6fb
jjol <jjoulk@gmail.com>
2020-07-07 17:46:17 -0500

    Remover Tokens y datos confidenciales

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,14 +2,14 @@ export const environment = {
   production: true,
   database: {
     firebaseConfig: {
-      apiKey: 'AIzaSyCkD2vo5Mnj4OdWZ3pCLG6rs5XQ_lucUxE',
-      authDomain: 'itesm-ca2020.firebaseapp.com',
-      databaseURL: 'https://itesm-ca2020.firebaseio.com',
-      projectId: 'itesm-ca2020',
-      storageBucket: 'itesm-ca2020.appspot.com',
-      messagingSenderId: '439394464089',
-      appId: '1:439394464089:web:70fcf668d6d3c888bd5c48',
-      measurementId: 'G-TY0WJ7QE7W'
+      apiKey: /* TODO: Get Firebase API SDK */'',
+      authDomain: '',
+      databaseURL: '',
+      projectId: '',
+      storageBucket: '',
+      messagingSenderId: '',
+      appId: '',
+      measurementId: ''
     }
   }
 };
